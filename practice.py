2d3e9e0342cbb9da915a3a64e023b3f6bdbab3e6
akkireddy768 <babhargav4@gmail.com>
2023-12-24 23:35:06 -0500

    Commit Practice

---
+++ practice.py
@@ -0,0 +1,12 @@
+def print_new(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print({name})  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_new('Hello')
+    print_new('Bhargava')
+    print_new('Akkireddy')
+    print_new('Lets practice')
+    print_new('Coding for Python')
